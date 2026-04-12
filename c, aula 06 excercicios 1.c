6c668f7ef049fef0d4cec8162a595a030517a186
BrunaElisaDeSouza <bs497153@gmail.com>
2026-04-12 15:47:33 -0300

    Add comments explaining triplo function and usage
    Added comments to clarify the purpose of the triplo function and the main program.

--- c, aula 06 excercicios 1.c
+++ c, aula 06 excercicios 1.c
@@ -1,3 +1,8 @@
+// Crie a função triplo em C, que recebe um número inteiro e
+//retorna o tripo do valor recebido.
+//Crie um programa em C que recebe um número do usuário
+//e mostra o tripo usando a função criada anteriormente.
+
 #include <stdio.h> 
 
 int triplo(int x); 
